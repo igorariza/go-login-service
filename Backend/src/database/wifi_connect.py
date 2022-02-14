10ff1f26e6b872f8ce80b10c4b04a08420c7193b
Igor Ariza <igorariza@MacBook-Pro-de-Igor.local>
2022-02-14 09:02:21 -0500

    feat: connection firebase

---
+++ Backend/src/database/wifi_connect.py
@@ -0,0 +1,17 @@
+# This file connects esp8266 with wifi
+# To connect, it want a SSID and Password
+
+def wlan_connect(ssid, password):
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    if not wlan.active() or not wlan.isconnected():
+        wlan.active(True)
+        print('connecting to:', ssid)
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    if wlan.isconnected():
+        print('succesfully connected to', ssid)
+    else:
+        print('connection to', ssid, 'failed')
+    print('network config:', wlan.ifconfig())
